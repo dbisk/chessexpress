53c728943e0f5ef055c3f7388232450b61c359fd
dbisk <16860792+dbisk@users.noreply.github.com>
2020-03-04 14:09:38 -0600

    (#27) Added some starter code for CircuitPython

---
+++ src/samd51/code.py
@@ -0,0 +1,12 @@
+"""Entry point to the CircuitPython code
+
+code.py is run when the processor boots up, given default configuration.
+"""
+
+# import CircuitPython modules
+
+
+# import user modules
+
+# "main function", but not really because microcontroller
+print("Hello World!")
---
+++ src/samd51/secrets_template.py
@@ -0,0 +1,10 @@
+# Rename this file to 'secrets.py', and replace the info below with the relevant
+# information for your setup
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info and sharing it
+secrets = {
+  'ssid' = 'the ssid',
+  'password' = 'wifi password',
+  'timezone' = 'America/Chicago',
+}
